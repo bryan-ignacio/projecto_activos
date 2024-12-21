d31ecd5229d6f480d7e759dd6494503cb959df6b
Bryan Ignacio <bryan-ignacio@outlook.com>
2024-12-21 16:33:54 -0600

    first commit

--- main.cpp
+++ main.cpp
@@ -5,6 +5,54 @@ using namespace std;
 
 int main() {
     MatrizDispersa *matriz = new MatrizDispersa();
+    Usuario *user1 = new Usuario("elian_estrada", "Elian Estrada", "1234", "guatemala", "igss");
+    Node *node1 = new Node(user1);
+    matriz->insertarValor(node1, node1->user->getDepartamento(), node1->user->getEmpresa());
+
+    Usuario *user2 = new Usuario("juanito", "Juan Perez", "4567", "jutiapa", "max");
+    Node *node2 = new Node(user2);
+    matriz->insertarValor(node2, node2->user->getDepartamento(), node2->user->getEmpresa());
+
+    Usuario *user3 = new Usuario("pedrito", "Pedro Rodriguez", "48956", "jalapa", "usac");
+    Node *node3 = new Node(user3);
+    matriz->insertarValor(node3, node3->user->getDepartamento(), node3->user->getEmpresa());
+
+    Usuario *user8 = new Usuario("mafer", "Maria Fernanda", "54312", "peten", "cinepolis");
+    Node *node8 = new Node(user8);
+    matriz->insertarValor(node8, node8->user->getDepartamento(), node8->user->getEmpresa());
+
+    Usuario *user4 = new Usuario("juanma", "Juan Manuel", "32897", "guatemala", "usac");
+    Node *node4 = new Node(user4);
+    matriz->insertarValor(node4, node4->user->getDepartamento(), node4->user->getEmpresa());
+
+    Usuario *user5 = new Usuario("casimiro", "Carlos Perez", "721896", "guatemala", "max");
+    user5->arbolActivos->insert(new Activo("balanza", ""));
+    user5->arbolActivos->insert(new Activo("canastas", ""));
+    user5->arbolActivos->insert(new Activo("linternas", ""));
+    user5->arbolActivos->insert(new Activo("cargadores", ""));
+    user5->arbolActivos->insert(new Activo("cables", ""));
+    user5->arbolActivos->insert(new Activo("tazos", ""));
+    Node *node5 = new Node(user5);
+    matriz->insertarValor(node5, node5->user->getDepartamento(), node5->user->getEmpresa());
+
+    Usuario *user6 = new Usuario("fuego03", "Fernando Mendez", "891346", "jutiapa", "cinepolis");
+    Node *node6 = new Node(user6);
+    matriz->insertarValor(node6, node6->user->getDepartamento(), node6->user->getEmpresa());
+
+    Usuario *user7 = new Usuario("azurdia", "Alejandra Guzman", "780145", "jutiapa", "usac");
+    Node *node7 = new Node(user7);
+    matriz->insertarValor(node7, node7->user->getDepartamento(), node7->user->getEmpresa());
+
+    Usuario *user9 = new Usuario("incrediboy", "Iraldo Martinez", "201598", "jutiapa", "max");
+    Node *node9 = new Node(user9);
+    matriz->insertarValor(node9, node9->user->getDepartamento(), node9->user->getEmpresa());
+
+    Usuario *user10 = new Usuario("alcachofa", "Antonio Lopez", "20435", "jalapa", "usac");
+    Node *node10 = new Node(user10);
+    matriz->insertarValor(node10, node10->user->getDepartamento(), node10->user->getEmpresa());
+    //casimiro
+
+
     int option;
     while (true) {
         cout << "=========================" << endl;
@@ -175,9 +223,11 @@ int main() {
                                 cout << "Modificar Activo: " << endl;
                                 cout << "mostrar lista de activos" << endl;
                                 usuarioEncontrado->user->arbolActivos->printActivos();
+
                                 cout << "Ingrese el IdActivo a Modificar: ---" << endl;
                                 cout << "ingrese descripcion nueva: " << endl;
                                 cout << "Activo Modificado:" << endl;
+
                                 cout << "ID: --- NOMBRE: --- DESCRIPCION: ---" << endl;
                                 break;
                             case 4:
